5bbe9da261fe535ba96fd9488bdb449c3140cd9b
UVvirus <39599249+UVvirus@users.noreply.github.com>
2020-05-07 21:13:48 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,5 @@
+miles_per_hour=float(input("Enter the speed:"))
+meter_per_second=miles_per_hour * 0.4474
+meter_per_second=round(meter_per_second,2)
+print("speed in meter per second:",meter_per_second)
+
